6bb96f6c215072b76a48d68738ca0230c8dddcc7
Eric Nguyen <42782243+GotamaHuy@users.noreply.github.com>
2019-12-16 13:29:06 -0700

    updated routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
-  resources :cars
-  resources :parts
-  resources :makes
+  resources :cars do
+    collection do
+      get 'search'
+    end
+  end
+  # search_cars GET /cars/search(.:format) cars#search
+  resources :makes do
+    collection do
+      get 'search'
+    end
+  end
+  # search_makes GET /makes/search(.:format) makes#search
+  resources :parts do
+    collection do
+      get 'search'
+    end
+  end
+  root 'index#cars'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
